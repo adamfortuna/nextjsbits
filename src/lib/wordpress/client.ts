59555b0b03efa1ad6f403d2b2479661491efbc67
Adam Fortuna <adam@fortuna.name>
2023-11-16 00:22:27 -0700

    Added unstable cache to try that out

--- src/lib/wordpress/client.ts
+++ src/lib/wordpress/client.ts
@@ -1,11 +1,7 @@
 import { GraphQLClient } from "graphql-request"
-import { cache } from "react"
 
 export const wordpressClient = new GraphQLClient(String(process.env.WP_NEXTBITS_GRAPHQL_URL), {
   headers: {
     authorization: `Basic ${String(process.env.WP_NEXTBITS_TOKEN)}`,
-  },
-  fetch: cache(async (url: any, params: RequestInit | undefined) =>
-    fetch(url, { ...params, cache: "force-cache", next: { revalidate: 600 } })
-  ),
+  }
 })
