de8f3dfe202cb6c126939fbf975099a98894fe1c
Adam Fortuna <adam@fortuna.name>
2023-11-15 18:24:07 -0700

    Force caching of graphql

--- src/lib/wordpress/client.ts
+++ src/lib/wordpress/client.ts
@@ -6,6 +6,6 @@ export const wordpressClient = new GraphQLClient(String(process.env.WP_NEXTBITS_
     authorization: `Basic ${String(process.env.WP_NEXTBITS_TOKEN)}`,
   },
   fetch: cache(async (url: any, params: RequestInit | undefined) =>
-    fetch(url, { ...params, next: { revalidate: 600 } })
+    fetch(url, { ...params, cache: "force-cache", next: { revalidate: 600 } })
   ),
 })
